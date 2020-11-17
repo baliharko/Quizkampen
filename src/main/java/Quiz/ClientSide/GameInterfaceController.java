eae5803d1160a9433e9e9a54aa6f6a02de26233b
baliharko <bali@fauxdelorean.com>
2020-11-17 09:51:32 +0100

    fixes

--- src/main/java/Quiz/ClientSide/GameInterfaceController.java
+++ src/main/java/Quiz/ClientSide/GameInterfaceController.java
@@ -9,12 +9,13 @@ public class GameInterfaceController {
 
     public Text questionText;
 
-    public Button button1;
+    public Button button1; // set text to answers
     public Button button2;
     public Button button3;
     public Button button4;
 
     public Text connectionStatusMessage;
+
     public StackPane questionArea;
     public GridPane buttonGrid;
 }

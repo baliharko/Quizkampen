209c89cb54b39431074bdd4b1ad61cbde9fa9940
baliharko <bali@fauxdelorean.com>
2020-11-17 11:26:23 +0100

    switched to toggle-buttons

--- src/main/java/Quiz/ClientSide/GameInterfaceController.java
+++ src/main/java/Quiz/ClientSide/GameInterfaceController.java
@@ -1,6 +1,7 @@
 package Quiz.ClientSide;
 
-import javafx.scene.control.Button;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.text.Text;
@@ -9,14 +10,31 @@ public class GameInterfaceController {
 
     public Text questionText;
 
-    public Button button1; // set text to answers
-    public Button button2;
-    public Button button3;
-    public Button button4;
+    public ToggleButton button1; // set text to answers
+    public ToggleButton button2;
+    public ToggleButton button3;
+    public ToggleButton button4;
 
     public Text connectionStatusMessage;
 
     public StackPane questionArea;
     public GridPane buttonGrid;
+
+    public ToggleGroup group;
+
+    public ToggleButton toggle1;
+    public ToggleButton toggle2;
+    public ToggleButton toggle3;
+    public ToggleButton toggle4;
+
+    public void initialize() {
+        group = new ToggleGroup();
+
+        toggle1.setToggleGroup(group);
+        toggle2.setToggleGroup(group);
+        toggle3.setToggleGroup(group);
+        toggle4.setToggleGroup(group);
+
+    }
 }
 

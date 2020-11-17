082848827e363b43edeec8f53553fa865a582075
rost2001 <robin.stempa@yh.nackademin.se>
2020-11-17 09:38:30 +0000

    version1

---
+++ src/main/java/sample/Question.java
@@ -0,0 +1,21 @@
+package sample;
+
+import java.io.Serializable;
+
+public class Question implements Serializable {
+    private String question;
+    private String answer;
+
+    public Question(String q, String a) {
+        this.question = q;
+        this.answer = a;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+}

4c65eeae96b159d2e7a2f8db5166662f40824ed0
Robin Martinsson <martinsson.r@gmail.com>
2020-11-26 10:33:44 +0100

    SelectCategory

---
+++ src/main/java/Quiz/ClientSide/controllers/SelectCategoryInterfaceController.java
@@ -0,0 +1,42 @@
+package Quiz.ClientSide.controllers;
+
+import javafx.scene.control.ToggleButton;
+
+import java.awt.event.ActionEvent;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+/**
+ * Created by Robin Martinsson
+ * Date:    2020-11-26
+ * Time:    10:18
+ * Project: Inlämning04
+ * Copyright: MIT
+ */
+public class SelectCategoryInterfaceController implements Serializable {
+
+    ObjectOutputStream out;
+
+    private ToggleButton c1Btn;
+    private ToggleButton c2Btn;
+    private ToggleButton c3Btn;
+
+    public void selectedCategory(ActionEvent event) throws IOException {
+
+        if  ((event.getSource()).equals(c1Btn)) {
+            System.out.println(c1Btn.getText());
+            String sendCategory = c1Btn.getText();
+            out.writeObject("Category " + sendCategory);
+        }
+                else if  ((event.getSource()).equals(c2Btn)) {
+            System.out.println(c2Btn.getText());
+            out.writeObject("Category " + c2Btn.getText());
+        }
+        else if  ((event.getSource()).equals(c3Btn)) {
+            System.out.println(c3Btn.getText());
+            out.writeObject("Category " + c3Btn.getText());
+        }
+    }
+
+}
